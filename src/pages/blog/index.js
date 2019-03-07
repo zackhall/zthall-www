fc515c300baa98d43e94e3e65aa6fa9b2567a7a5
Zack Hall <zackhall@outlook.com>
2019-03-06 21:18:08 -0500

    Remove extraneous components

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
 import Layout from '../../components/Layout'
-import BlogRoll from '../../components/BlogRoll'
 
 export default class BlogIndexPage extends React.Component {
   render() {
-    
+
   return (
       <Layout>
         <section className="section">
@@ -30,8 +29,7 @@ export default class BlogIndexPage extends React.Component {
                 </h1>
               </div>
             </div>
-              <BlogRoll />
-            </div>
+          </div>
         </section>
       </Layout>
     )
