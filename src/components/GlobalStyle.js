c1db4799dab5120e03c06ad34da4fc615b5d033f
Zack Hall <zackhall@outlook.com>
2019-03-07 22:07:11 -0500

    Update primary color to richer blue

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -3,11 +3,11 @@ import { mobile, phone } from '../utils/media'
 
 export default createGlobalStyle`
   :root {
-    --color-primary: #3956A3;
-    --color-background: #FFFFFF;
-    --color-neutral: rgba(51, 51, 51, 1.0);
-    --color-neutral-light: rgba(200, 200, 200, 1.0);
-    --color-neutral-xlight: rgba(240, 240, 240, 1.0);
+    --color-primary: rgba(17, 70, 206, 1.00);
+    --color-background: rgba(255, 255, 255, 1.00);
+    --color-neutral: rgba(51, 51, 51, 1.00);
+    --color-neutral-light: rgba(200, 200, 200, 1.00);
+    --color-neutral-xlight: rgba(240, 240, 240, 1.00);
 
     --font-family-primary: Helvetica, Arial, sans-serif;
   }
