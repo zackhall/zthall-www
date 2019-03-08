1ab6297ff48ee614582f559830d4732487899e8c
Zack Hall <zackhall@outlook.com>
2019-03-07 22:34:27 -0500

    Simplify footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,113 +1,54 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
+import dribbble from '../img/social/dribbble.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
 import vimeo from '../img/social/vimeo.svg'
 
+const FooterContainer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  color: var(--color-neutral-light);
+`
+
+const SocialIcon = styled.img`
+  width: 1em;
+  height: 1em;
+  opacity: 0.5;
+  margin: 1rem;
+  &:hover {
+    cursor: pointer;
+    opacity: 1;
+    filter: invert(0.3) sepia(1) saturate(60) hue-rotate(200deg);
+  }
+`
+
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
+      <FooterContainer>
+        <div>
+          <a title="dribbble" href="https://facebook.com">
+            <SocialIcon src={dribbble} alt="Dribbble" />
+          </a>
+          <a title="twitter" href="https://twitter.com">
+            <SocialIcon src={twitter} alt="Twitter" />
+          </a>
+          <a title="instagram" href="https://instagram.com">
+            <SocialIcon src={instagram} alt="Instagram" />
+          </a>
+          <a title="vimeo" href="https://vimeo.com">
+            <SocialIcon src={vimeo} alt="Vimeo" />
+          </a>
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
+        <div>
+          <small>Copyright © 2019 Zack Hall</small>
         </div>
-      </footer>
+      </FooterContainer>
     )
   }
 }
