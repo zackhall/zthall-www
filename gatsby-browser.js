4f714b98cec40d59444620892cc905429434051a
Zack Hall <zack@zthall.com>
2025-10-27 21:21:14 -0400

    chore: remove all old gatsby/react dependencies

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-// custom typefaces
-import 'typeface-inter';
-import 'typeface-space-mono';
-
-import './src/styles/prism-gh.css';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
-import './src/styles/prism-mods.css';
-import './src/styles/styles.css';
